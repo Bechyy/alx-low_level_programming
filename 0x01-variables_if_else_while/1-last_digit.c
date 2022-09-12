0cde40da9240417597411ee9214a604482d09eca
Bechyy <bereketbeyene344@gmail.com>
2022-09-12 06:38:49 -0700

    last digitt

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,35 +1,55 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
 /**
-* main - print if the number is positive, zero, or negative
+
+* main - print if the number is positive, zero, or negative 
+
 *
-* Description: using the main function
+
+* Description: using the main function 
+
 * this program prints "programming is positive, zero, or negative
+
 * Return: 0
+
 */
+
 int main(void)
+
 {
-int n;
-int l;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-l = n % 10;
-if (l > 5)
-{
-<<<<<<< HEAD
-printf("Last digit of %d is %d and is greater than 5\n", n, l);
-}
-else if (l == 0)
-{
-printf("Last digit of %d is %d and is 0\n", n, l);
-}
-else 
-{
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-}
-return (0);
-=======
-printf("Last digit of %d and is greater than 5\n", n, l);
->>>>>>> origin/master
-}
+
+    int n;
+
+    int L;
+
+    char str[] = "Last digit of";
+
+
+
+    srand(time(0));
+
+    n = rand() - RAND_MAX / 2;
+
+    L = n % 10;
+
+
+
+    if (L > 5)
+
+    printf("%s %d is %d and is greater than 5\n", str, n, L);
+
+    else if (L == 0)
+
+    printf("%s %d is %d and is 0\n", str, n, L);
+
+    else if (L < 6)
+
+    printf("%s %d is %d and is less than 6 and not 0\", str , n, L);
+
+    return (0); 
+
+    }
