f1e60dbbaea3c61638d12d06dfc04e7bd344ffcc
Bechyy <bereketbeyene344@gmail.com>
2022-09-13 05:33:56 -0700

    islower

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,27 @@
+nclude “main.h”                                                                                                                   
+
+/**                                                                                                                                 
+ *
+ * * main - check the code                                                                                                             
+ *
+ * *                                                                                                                                   
+ *
+ * * Return: Always 0                                                                                                                  
+ *
+ * */                                                                                                                                  
+
+int _islower(int c)                                                                                                                 
+
+{                                                                                                                                   
+
+	if (c >= 'a' && c <= 'z')                                                                                                           
+
+		return (1);                                                                                                                         
+
+	else                                                                                                                                
+
+		return (0);                                                                                                                         
+
+}
+
+
