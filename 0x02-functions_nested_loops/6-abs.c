20ef3bfaa1e44df4ca9f5d984bde8705c1bb45ce
Bechyy <bereketbeyene344@gmail.com>
2022-09-14 12:32:37 -0700

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,33 @@
-#include "main.h"
-/**
-* _abs - function that prints a absolute value of number
-* @n: takes in an integer
-* Return: 1if greater than 0, 0 if zero and -1 if less than 0
-*/
-int _abs(int n)
+#include "main.h"                                                                                                                   
+
+                                                                                                                                    
+
+/**                                                                                                                                 
+
+                                                                                                                                    
+
+* _abs - function that prints a absolute value of number                                                                            
+
+                                                                                                                                    
+
+* @n: takes in an integer                                                                                                           
+
+                                                                                                                                    
+
+* Return: 1if greater than 0, 0 if zero and -1 if less than 0                                                                       
+
+                                                                                                                                    
+
+*/                                                                                                                                  
+
+                                                                                                                                    
+
+int _abs(int n)                                                                                                                     
+
+                                                                                                                                    
+
 {
-if(n < 0 )
-return (n * -1);
+if (n < 0)
+n = -n;
 return (n);
 }
