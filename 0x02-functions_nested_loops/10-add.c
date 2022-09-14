0c7222d0e0caa609b59734a519577fb594b19ba8
Bechyy <bereketbeyene344@gmail.com>
2022-09-14 13:04:43 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"                                                                                                                   
+
+/**                                                                                                                                 
+
+* A function that prints last digit                                                                                                 
+
+* @n: takes in an integer                                                                                                           
+
+* Return: result output                                                                                                             
+
+*/
+int add(int, int)
+{
+int 5;
+int 8;
+result = 5 + 8
+printf("%d\n", result);
+return (0);
+}
